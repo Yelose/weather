17b35a7d4cd647b5573cee5aabb17f2093bdec45
Marina González Suárez <yelose85@gmail.com>
2022-11-03 11:57:47 +0100

    app working, no css file, input the city and it outputs the temperature and the description of the day

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 const express = require("express");
 const https = require("https");
-
+const bodyParser = require("body-parser");
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function (req, res) {
-  res.send("Server is up and running.");
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  const city = req.body.city;
+  const apiKey = "a4e6d4d787b23e7b98da5dee02a34d3f";
+  const unit = "metric";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+
+  https.get(url, function (response) {
+    console.log(response.statusCode);
+    response.on("data", function (data) {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const description = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+      res.write("<p>The weather is currently " + description + "</p>");
+      res.write(
+        `<h1>The temperature in ${city} is ${temp} degrees Celsius.</h1>`
+      );
+      res.write(`<img src="${iconURL}">`);
+      res.send();
+    });
+  });
 });
 
 app.listen(3000, function () {
